fd91ebb68c51444101f66985144d7efa7c3f9552
Marc Sanchez Net <marc.sanchez.net@jpl.nasa.gov>
2021-04-02 12:11:31 -0700

    Remove unfinished function from admin

--- pyion/_admin.c
+++ pyion/_admin.c
@@ -61,15 +61,6 @@ static char delete_contact_docstring[] =
     "Delete contact(s) in ION's contact plan.";
 static char delete_range_docstring[] =
     "Delete range(s) in ION's contact plan.";
-static char ltp_span_exists_docstring[] =
-    "Check if an LTP span is defined in ION.";
-static char ltp_update_span_docstring[] =
-    "Update/Modify an LTP span configuration.";
-static char ltp_info_span_docstring[] =
-    "Get information about an LTP span configuration.";
-static char cfdp_pdu_size_docstring[] =
-    "Update/Modify the CFDP segment/PDU size.";
-
 
 // Declare the functions to wrap
 static PyObject *pyion_bp_watch(PyObject *self, PyObject *args);
@@ -81,10 +72,6 @@ static PyObject *pyion_add_contact(PyObject *self, PyObject *args);
 static PyObject *pyion_add_range(PyObject *self, PyObject *args);
 static PyObject *pyion_delete_contact(PyObject *self, PyObject *args);
 static PyObject *pyion_delete_range(PyObject *self, PyObject *args);
-static PyObject *pyion_ltp_span_exists(PyObject *self, PyObject *args);
-static PyObject *pyion_ltp_update_span(PyObject *self, PyObject *args);
-static PyObject *pyion_ltp_info_span(PyObject *self, PyObject *args);
-static PyObject *pyion_cfdp_pdu_size(PyObject *self, PyObject *args);
 
 // Define member functions of this module
 static PyMethodDef module_methods[] = {
@@ -97,10 +84,6 @@ static PyMethodDef module_methods[] = {
     {"add_range", pyion_add_range, METH_VARARGS, add_range_docstring},
     {"delete_contact", pyion_delete_contact, METH_VARARGS, delete_contact_docstring},
     {"delete_range", pyion_delete_range, METH_VARARGS, delete_range_docstring},
-    {"ltp_span_exists", pyion_ltp_span_exists, METH_VARARGS, ltp_span_exists_docstring},
-    {"ltp_update_span", pyion_ltp_update_span, METH_VARARGS, ltp_update_span_docstring},
-    {"ltp_info_span", pyion_ltp_info_span, METH_VARARGS, ltp_info_span_docstring},
-    {"cfdp_pdu_size", pyion_cfdp_pdu_size, METH_VARARGS, cfdp_pdu_size_docstring},
     {NULL, NULL, 0, NULL}
 };
 
@@ -487,196 +470,6 @@ static PyObject *pyion_delete_range(PyObject *self, PyObject *args) {
 
     // Delete the contact(s)
     oK(rfx_remove_range(&fromTime, fromNode, toNode));
-
-    Py_RETURN_NONE;
-}
-
-/* ============================================================================
- * === LTP administration functions
- * ============================================================================ */
-
-static int pyion_find_span(uvast engineNbr, PsmAddress *elt) {
-    // Define variables
-    Sdr      sdr = getIonsdr();
-    LtpVspan *vspan;
-
-    // If not sdr, set exception and return
-    if (sdr == NULL) {
-        pyion_SetExc(PyExc_RuntimeError, "Cannot find SDR.");
-        return 0;
-    }
-
-    // Find the enpoint
-    if (!sdr_pybegin_xn(sdr)) return 0; 
-    findSpan(engineNbr, &vspan, elt);
-    sdr_exit_xn(sdr);
-
-    return 1;
-}
-
-static PyObject *pyion_ltp_span_exists(PyObject *self, PyObject *args) {
-    // Attach to ION
-    if (!py_ltp_attach()) return NULL;
-
-    // Define variables
-    PsmAddress elt;
-    unsigned long long nbr;
-    
-    // Parse the input tuple. Raises error automatically if not possible
-    if (!PyArg_ParseTuple(args, "K", &nbr)) goto error;
-
-    // Find the span
-    if (!pyion_find_span((uvast)nbr, &elt)) goto error;
-
-    // See if found
-    if (elt) goto found; else goto not_found;
-
-error:
-    return NULL;
-found:
-    Py_RETURN_TRUE;
-not_found:
-    Py_RETURN_FALSE;
-}
-
-static PyObject *pyion_ltp_update_span(PyObject *self, PyObject *args) {
-    // Attach to ION
-    if (!py_ltp_attach()) return NULL;
-
-    // Define variables
-    unsigned long long nbr;
-    unsigned int maxExportSessions, maxImportSessions;
-    unsigned int segmentSize, aggrSizeLimit, aggrTimeLimit;
-    char *lsoCmd = NULL;
-    unsigned int qTime;
-    int purge, ok;
-    PsmAddress elt;
-    Sdr sdr = getIonsdr();
-
-    // Parse the input tuple. Raises error automatically if not possible
-    if (!PyArg_ParseTuple(args, "KIIIIIsIi", &nbr, &maxExportSessions, &maxImportSessions, &segmentSize,
-                            &aggrSizeLimit, &aggrTimeLimit, &lsoCmd, &qTime, &purge))
-        goto error;
-
-    // Check if this span exists
-    if (!pyion_find_span((uvast)nbr, &elt)) goto error;
-
-    // If a span to this engine number does not exists, raise error
-    if (!elt) {
-        pyion_SetExc(PyExc_ValueError, "No LTP span for engine %llu.", nbr);
-        goto error;
-    }
-
-    // Trigger span update
-    ok = updateSpan(nbr, maxExportSessions, maxImportSessions, segmentSize, aggrSizeLimit,
-                    aggrTimeLimit, lsoCmd, qTime, purge);
-
-    // If error, raise exception
-    if (!ok) {
-        pyion_SetExc(PyExc_RuntimeError, "Error while updating span.");
-        goto error;
-    }
-
-    goto ok;
-
-error:
-    return NULL;
-ok:
-    Py_RETURN_NONE;
-}
-
-static PyObject *pyion_ltp_info_span(PyObject *self, PyObject *args) {
-    // Attach to ION
-    if (!py_ltp_attach()) return NULL;
-
-    // Initialize variables
-    unsigned long long nbr;
-    Sdr		        sdr = getIonsdr();
-	LtpVdb		    *vdb = getLtpVdb();
-	PsmPartition	ionwm = getIonwm();
-    Object		    ltpdbObj = getLtpDbObject();
-	PsmAddress	    elt;
-	LtpVspan	    *vspan;
-    char            lso_cmd[256];
-    //OBJ_POINTER(LtpDB, ltpdb);      // Defines the variable ltpdb as type LtpDB
-    OBJ_POINTER(LtpSpan, span);     // Defines the variable ``span`` as type ``LtpSpan``
-
-    // Parse the input tuple. Raises error automatically if not possible
-    if (!PyArg_ParseTuple(args, "K", &nbr)) goto error;
-
-    // Initialize empty Python list
-    PyObject* py_spans = PyList_New(0);
-
-    // Start SDR transaction
-    if (!sdr_pybegin_xn(sdr)) goto error;
-    //GET_OBJ_POINTER(sdr, LtpDB, ltpdb, ltpdbObj);
-
-    // Iterate over all spans
-    for (elt = sm_list_first(ionwm, vdb->spans); elt; elt = sm_list_next(ionwm, elt)) {
-        // Get the span data from memory
-        vspan = (LtpVspan *) psp(ionwm, sm_list_data(ionwm, elt));
-
-        // If a number has been provided, use it to filter the spans
-        if (nbr > 0 && nbr != vspan->engineId) continue;
-
-        // Read the LSO command
-        GET_OBJ_POINTER(sdr, LtpSpan, span, sdr_list_data(sdr, vspan->spanElt));
-        sdr_string_read(sdr, lso_cmd, span->lsoCmd);
-
-        // Save span  information
-        PyList_Append(py_spans, Py_BuildValue(py_span_def, "engine_nbr", vspan->engineId, "max_export_sessions",
-                                              span->maxExportSessions, "max_import_sessions", span->maxImportSessions,
-                                              "agg_size_limit", span->aggrSizeLimit, "agg_time_limit", span->aggrTimeLimit,
-                                              "max_segment_size", span->maxSegmentSize, "lso_cmd", span->lsoCmd,
-                                              "lso_pid", vspan->lsoPid, "lsi_pid", vdb->lsiPid, "q_lat", span->remoteQtime, 
-                                              "purge", span->purge));
-    }
-
-    // Goto ok
-    goto ok;
-
-error:
-    sdr_exit_xn(sdr);
-    return NULL;
-ok:
-    sdr_exit_xn(sdr);
-    return py_spans;
-}
-
-/* ============================================================================
- * === CFDP administration functions
- * ============================================================================ */
-
-static PyObject *pyion_cfdp_pdu_size(PyObject *self, PyObject *args) {
-    // Attach to ION
-    if (!py_cfdp_attach()) return NULL;
-
-    // Defined variables
-    Sdr     sdr = getIonsdr();
-    Object	cfdpdbObj = getCfdpDbObject();
-	CfdpDB	cfdpdb;
-    int     segsize;
-
-    // Parse the input tuple. Raises error automatically if not possible
-    if (!PyArg_ParseTuple(args, "i", &segsize))
-        return NULL;
-
-    // Check validity of inputs
-    if (!sdr) {
-        pyion_SetExc(PyExc_RuntimeError, "Cannot find SDR.");
-        return NULL;
-    }
-    if (!cfdpdbObj || cfdpdbObj == (Object)ERROR) {
-        pyion_SetExc(PyExc_RuntimeError, "Cannot find local CFDP engine.");
-        return NULL;
-    }
-
-    // Modify the segment size
-    if (!sdr_pybegin_xn(sdr)) return NULL;
-    sdr_stage(sdr, (char *) &cfdpdb, cfdpdbObj, sizeof(CfdpDB));
-	cfdpdb.maxFileDataLength = segsize;
-	sdr_write(sdr, cfdpdbObj, (char *) &cfdpdb, sizeof(CfdpDB));
-    if (!sdr_pyend_xn(sdr)) return NULL;
 
     Py_RETURN_NONE;
 }
