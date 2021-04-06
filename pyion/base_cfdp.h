48d2eb63ffca77d54588e42cfa1e875b22a4a67b
Christian Aguilar <cbaguilar@ucla.edu>
2021-04-05 20:50:27 -0700

    Added new typedef for cfdpReqParms

--- pyion/base_cfdp.h
+++ pyion/base_cfdp.h
@@ -4,16 +4,25 @@
 #include <cfdp.h>
 #include <bputa.h>
 
-typedef struct {
-	CfdpHandler		    faultHandlers[16];
-	CfdpNumber		    destinationEntityNbr;
-	char			    *sourceFileName;
-	char			    destFileNameBuf[256];
-	char			    *destFileName;
-	BpUtParms		    utParms;
-	unsigned int		closureLatency;
-	CfdpMetadataFn		segMetadataFn;
-	MetadataList		msgsToUser;
-	MetadataList		fsRequests;
-	CfdpTransactionId	transactionId;
+typedef struct
+{
+    CfdpHandler faultHandlers[16];
+    CfdpNumber destinationEntityNbr;
+    char *sourceFileName;
+    char destFileNameBuf[256];
+    char *destFileName;
+    BpUtParms utParms;
+    unsigned int closureLatency;
+    CfdpMetadataFn segMetadataFn;
+    MetadataList msgsToUser;
+    MetadataList fsRequests;
+    CfdpTransactionId transactionId;
 } CfdpReqParms;
+
+int base_cfdp_attach();
+
+void base_cfdp_detach();
+
+int base_cfdp_compress_number();
+
+int base_cfdp_open();
