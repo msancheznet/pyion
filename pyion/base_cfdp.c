97a449a76ae917e3958b0da19c9f0461fc307907
Christian Aguilar <cbaguilar@ucla.edu>
2021-04-05 20:50:15 -0700

    Added ommitted shim-layer function

--- pyion/base_cfdp.c
+++ pyion/base_cfdp.c
@@ -13,11 +13,20 @@
  * from a particular uvast. Then, apply the appropriate 
  * anciallyData flags based off of our given criticality.
  */
-int base_cfdp_open(CfdpReqParams *params, uvast from, int criticality) {}
+int base_cfdp_open(CfdpReqParms *params, uvast from, int criticality) {
     cfdp_compress_number(&(params->destinationEntityNbr), from);
     if (criticality == 1)
 		params->utParms.ancillaryData.flags |= BP_MINIMUM_LATENCY;
 	else
 		params->utParms.ancillaryData.flags &= (~BP_MINIMUM_LATENCY);
 
+    return 0;
+}
+
+int base_cfdp_attach() {
+    return cfdp_attach();
+}
+
+void base_cfdp_detach() {
+    return cfdp_detach();
 }
