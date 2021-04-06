08e3df43328d0b0715dcc832a8fcf964fe2f6d07
Christian Aguilar <cbaguilar@ucla.edu>
2021-04-05 17:32:28 -0700

    Added translation of base_cfdp_open

--- pyion/base_cfdp.c
+++ pyion/base_cfdp.c
@@ -1,5 +1,23 @@
+#include <cfdp.h>
+#include "base_cfdp.h"
 /**
  * Worker file to handle pure C function
  * interaction between PyION and ION.
  * 
  */
+
+
+/**
+ * 
+ * cfdp param initialiation function. Compress the destination eid 
+ * from a particular uvast. Then, apply the appropriate 
+ * anciallyData flags based off of our given criticality.
+ */
+int base_cfdp_open(CfdpReqParams *params, uvast from, int criticality) {}
+    cfdp_compress_number(&(params->destinationEntityNbr), from);
+    if (criticality == 1)
+		params->utParms.ancillaryData.flags |= BP_MINIMUM_LATENCY;
+	else
+		params->utParms.ancillaryData.flags &= (~BP_MINIMUM_LATENCY);
+
+}
