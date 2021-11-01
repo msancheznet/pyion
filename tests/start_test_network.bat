7f8371cdf0903dd31a798b30f75a3829082ab467
Marc Sanchez Net <marc.sanchez.net@jpl.nasa.gov>
2021-11-01 16:30:51 -0700

    Update bat file to start Docker network

--- tests/start_test_network.bat
+++ tests/start_test_network.bat
@@ -1,16 +1,15 @@
 :: =====================================================================
-:: Start a network of Docker containers each of which
-:: has an ION node.
+:: Start a network of Docker containers. Each of which has an ION node.
 ::
 :: Parameters
 :: ----------
-::   %1 = Name of the docker-compose file (default is
-::        ion_compose.yaml)
+::   %1 = Name of test folder. Defaults to ``bp_tests``
 ::
 :: Usage
 :: -----
-:: C:\>.\connect_to 1
-:: root@ion_node1:~#
+:: C:\> start_test_network bp_tests
+:: C:\> start_test_network ltp_tests
+:: C:\> start_test_network cfdp_tests
 ::
 :: Author: Marc Sanchez Net
 :: 01/07/2019
@@ -19,7 +18,12 @@
 @echo off
 
 :: Set Docker-compose file
-set filename="./bp_tests/test_network.yaml"
+if "%1"=="" (
+	set filename="./bp_tests/test_network.yaml"
+) else (
+	set filename="./%1/test_network.yaml"
+)
+echo %filename%
 
 :: Clean up docker
 docker rm ion_node1
